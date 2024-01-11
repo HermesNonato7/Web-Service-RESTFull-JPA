5abd1c1c9ff69701680ca590be3e4408396fa0fe
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-11 02:19:20 -0300

    melhorando as exceptions

--- springweb/src/main/java/com/minhaempresa/springweb/application/services/exceptions/ResourceNotFoundException.java
+++ springweb/src/main/java/com/minhaempresa/springweb/application/services/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,7 @@
 package com.minhaempresa.springweb.application.services.exceptions;
 
-import java.io.Serial;
-
 public class ResourceNotFoundException extends RuntimeException {
-    @Serial
+
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Object id) {
